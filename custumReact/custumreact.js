34e2e835836245e724bf2007cebfdbef406e9661
Annu9111 <aksoni911190@gmail.com>
2026-09-25 06:53:11 +0530

    internal react

--- custumReact/custumreact.js
+++ custumReact/custumreact.js
@@ -1,1 +1,29 @@
- 
+function customRender(reactEle,container){
+    /*
+    const domEle = document.createElement(reactEle.type)
+    domEle.innerHTML = reactEle.children
+    domEle.setAttribute('href',reactEle.props.href)
+    domEle.setAttribute('target',reactEle.props.target)
+    container.append(domEle)
+    */
+
+    const domEle = document.createElement(reactEle.type);
+    domEle.innerHTML = reactEle.children;
+    for(const prop in reactEle.props){
+        if(prop==='children') continue;
+        domEle.setAttribute(prop,reactEle.props[prop])
+    }
+    container.appendChild(domEle);
+}
+
+const reactElement = {
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children :'click me to visit google'
+}
+const mainContainer  = document.querySelector("#root");
+
+customRender(reactElement,mainContainer);
